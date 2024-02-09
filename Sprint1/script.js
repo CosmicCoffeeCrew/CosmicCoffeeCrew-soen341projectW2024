acaa6d0dc79a47512d4fc55e0017b45bddb52657
Sara Amri <93613026+kokkuri3@users.noreply.github.com>
2024-02-09 10:47:15 -0500

    Added CSS and js to homepage draft

---
+++ Sprint1/script.js
@@ -0,0 +1,31 @@
+/*drop down menu for login button*/
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+/*carousel sliding mechanic*/
+var carousel = document.querySelector('.carousel');
+var cellCount = 9;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
