e15c16fabce3d3a456aee34009c4cb40a0f6b503
irizhukova <ira.zhu.2003@gmail.com>
2024-03-08 18:21:25 -0500

    home page modifications

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,26 +6,24 @@ import {
 
 const Footer = () => {
   return (
-    <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
+    <div className="bg-secondary-100 dark:bg-dark mt-14 rounded-t-3xl items-center">
       <section className="container">
         <div className=" grid md:grid-cols-3 py-5">
-          {/* company Details */}
           <div className=" py-8 px-4 ">
             <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif">
-              Car Rental
+              ZoomViteRapide
             </h1>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Possimus, voluptate.{" "}
+            <p className="text-sm text-left">
+              Contact us for all your inquiries{" "}
             </p>
             <br />
             <div className="flex items-center gap-3">
               <FaLocationArrow />
-              <p>Noida, Uttar Pradesh</p>
+              <p>Montreal, Quebec</p>
             </div>
             <div className="flex items-center gap-3 mt-3">
               <FaMobileAlt />
-              <p>+91 123456789</p>
+              <p>+1 (438)876-9054</p>
             </div>
           </div>
         </div>
