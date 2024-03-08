5de83c52637c4c7717e626655ed0af8fc16c7f43
hossamgasser <hossgasser@gmail.com>
2024-03-08 18:36:57 -0500

    user controls

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const MONGO_URI = 'mongodb+srv://cosmiccoffeecrew:kaicBr8bsVO5zK1a@cosmiccluster.ffvw5je.mongodb.net/?retryWrites=true&w=majority&appName=CosmicCluster'
+const PORT = 4000
+// remove mongo_URI and PORT and use them from the .env file but rn it s not working so i m keeping them here JAD A
+
+
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const vehicleRoutes = require('./routes/vehicles')
+const userRoutes = require('./routes/users')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path ,req.method)
+    next()
+})
+
+// Routes
+app.use('/api/vehicles',vehicleRoutes)
+app.use('/api/users',userRoutes)
+
+// connect to db
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        // listen to requests
+        app.listen(PORT, () =>{
+           console.log('connected to Db && listening on port', PORT)
+        })
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
