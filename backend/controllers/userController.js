f537f03e3d0d7c3237d44f3f6290bf39acf429e9
C4LIP0 <78159640+C4LIP0@users.noreply.github.com>
2024-03-10 14:31:53 -0400

    Sign-in tockens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,50 +1,48 @@
-const User = require('../models/userModel')
-const SECRET= "AFgfsdHJHjlhLFJEWpiBDIgdDHldYTODIUgodluIDFGldJFD"
-const jwt = require('jsonwebtoken')
+//Enrique: I modified this fild to implement the Email & Password Validation
 
-const createToken = (_id) => {
-    return jwt.sign({_id}, SECRET, {expiresIn: '3d'})
+const User = require ('.../models/userMoodel')
+const jwt = require("jsonwebtoken")
 
+const createTocken = (id) => {
+
+    jwt.sign({id}, process.env.SECRET, {expiresIn: '30d'}) //LOOK AT THE .ENV file
 }
-
 //login user
 const loginUser = async(req,res) => {
-
-    const{email, password} = req.body
-    try{
-        const user = await User.login(email, password)
-
-        //create a token
-        const token = createToken(user._id)
-        res.status(200).json({email, token})
-
-    }
-    catch (error) {
-        res.status(400).json({error: error.message})
-    }
-
-
+    res.json ({mssg: 'login user'})
 }
-
 
 //signup user
-const signupUser = async(req,res) => {
-
-
-    const {email, password, permission, License} = req.body 
+//tocken authentification
+const signupUser = async (req,res)=> {
+    const {email,password} = req.body
     try{
-        const user = await User.signup(email, password, permission, License)
+        const user = await User.signup(email,password)
 
-        //create a token
-        const token = createToken(user._id)
-        res.status(200).json({email, token, permission})
+        //Create a tocken 
+        const token = createTocken(user.id)
 
-    }
-    catch (error) {
-        res.status(400).json({error: error.message})
+        //res.status(200).json({email, user})
+        res.status(200).json({email, token})
+    }catch(error){
+        res.status(400).json({error:error.message})
     }
 }
 
-module.exports = { signupUser, loginUser}
+module.exports = {signupUser, loginUser}
+// //login user
+// const loginUser = async(req,res) => {
+//     res.json({mssg: 'login user'})
+
+// }
 
 
+// //signup user
+// const signupUser = async(req,res) => {
+//     res.json({mssg: 'signup user'})
+
+// }
+
+// modules.exports = { signupUser, loginUser}
+
+ 
