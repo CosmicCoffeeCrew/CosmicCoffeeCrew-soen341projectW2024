68470359475c23bc13e0f19d084794e8c2475427
Sara Amri <93613026+kokkuri3@users.noreply.github.com>
2024-03-11 23:33:50 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
+import ProductCatalog from "./components/Catalogue/ProductCatalogue";
+import products from './components/Catalogue/ProductDetails'; // Import products array
 
 const App = () => {
   // dark mode start
@@ -42,6 +44,7 @@ const App = () => {
       <Hero theme={theme} />
       <About />
       <Services />
+      <ProductCatalog products={products} />
       <Footer />
     </div>
   );
