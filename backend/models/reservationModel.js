1769e984c734278b54b43cad7fd2d7b0f77781aa
C4LIP0 <78159640+C4LIP0@users.noreply.github.com>
2024-03-14 15:59:15 -0400

    putting back-end together

---
+++ backend/models/reservationModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose")
+
+const User = require ('./userModel')
+const Vehicle = require ('./vehicleModel')
+const Schema = mongoose.Schema
+
+const reservationSchema = new Schema({
+    userID: {
+        type: String,
+        required: true
+    },
+    vehicleID: {
+        type: String,
+        required: true
+    },
+    start_Date: {
+        type: Date,
+        required: true
+    },
+    end_Date: {
+        type: Date,
+        required: true
+    },
+    charge: {
+        type: String,    //make integer later
+        required: true,
+    },
+    status :{
+        type : String,
+        enum:["pending","accepted","refused"],
+        required:true
+    }
+
+})
+
+
+reservationSchema.statics.record = async function(userID, vehicleID, start_Date, end_Date, charge, status){
+
+
+    // validation
+    if (!userID || !vehicleID || !start_Date || !end_Date || !charge ||!status){
+        throw Error("All fields must be filled")
+    }
+    const user_Exists = await User.findById(userID)
+
+    if(!user_Exists){
+        return res.status(404).json({error: 'No such User'})
+    }
+
+    const vehicle_Exists = await Vehicle.findById(vehicleID)
+
+    if(!vehicle_Exists){
+        return res.status(404).json({error: 'No such Vehicle'})
+    }
+
+
+    const reservation = await this.create({userID, vehicleID, start_Date, end_Date, charge, status})
+
+    return reservation
+}
+module.exports = mongoose.model('Reservation', reservationSchema)
