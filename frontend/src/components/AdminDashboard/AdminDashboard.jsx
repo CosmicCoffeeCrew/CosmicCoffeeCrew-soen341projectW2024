d917d938da1ed0ab164bdb9be9f41cf328a7c5a5
Sara Amri <sarang6721@gmail.com>
2024-03-21 20:29:21 -0400

    Implemented Update Operations on Vehicles and Users

--- frontend/src/components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import "./AdminDashboard.css";
+import PropTypes from 'prop-types';
 
 const AdminDashboard = () => {
     const [showVForm, setShowVForm] = useState(false);
@@ -10,7 +11,7 @@ const AdminDashboard = () => {
     const [users, setUsers] = useState([]);
     const [vehicleError, setVehicleError] = useState(null);
     const [userError, setUserError] = useState(null);
-
+    const [showSuccessPopup, setShowSuccessPopup] = useState(false);
     const [formData, setFormData] = useState({
         make: '',
         model: '',
@@ -34,7 +35,11 @@ const AdminDashboard = () => {
         birthdate: '',
         rentalHistory: ''
     });
-    
+    const [showVEdit, setShowVEdit] = useState(null);
+    const [showUEdit, setShowUEdit] = useState(null);
+    const [editedVehicle, setEditedVehicle] = useState(null); // New state to store edited vehicle
+    const [editedUser, setEditedUser] = useState(null); // New state to store edited user
+
 
     useEffect(() => {
         fetchVehicles();
@@ -76,6 +81,10 @@ const AdminDashboard = () => {
         setShowUForm(type === 'uForm');
         setShowVTable(type === 'vTable');
         setShowUTable(type === 'uTable');
+        
+        //if any button gets clicked, edit form disapears
+        setShowVEdit(false);
+        setShowUEdit(false);
     };
 
     const handleChange = (e) => {
@@ -89,6 +98,44 @@ const AdminDashboard = () => {
         setUserFormData({
             ...userFormData,
             [e.target.name]: e.target.value
+        });
+    };
+
+    const handleEditVehicle = (vehicle) => {
+        console.log('Editing vehicle:', vehicle);
+        setShowVEdit(true);
+        setEditedVehicle(vehicle);
+        // Autofill the fields with the data of the selected vehicle
+        setFormData({
+            make: vehicle.make || '',
+            model: vehicle.model || '',
+            year: vehicle.year || '',
+            type: vehicle.type || '',
+            color: vehicle.color || '',
+            mileage: vehicle.mileage || '',
+            transmission: vehicle.transmission || '',
+            location: vehicle.location || '',
+            fuelType: vehicle.fuelType || '',
+            seats: vehicle.seats || '',
+            pricePerDay: vehicle.pricePerDay || '',
+            image: vehicle.image || '',
+            description: vehicle.description || ''
+        });
+    };
+    
+    const handleEditUser = (user) => {
+        console.log('Editing user:', user);
+        setShowUEdit(true);
+        setEditedUser(user); 
+        // Autofill the fields with the data of the selected user
+        setUserFormData({
+            email: user.email || '',
+            password: user.password || '',
+            username: user.username || '',
+            permission: user.permission || 'Customer',
+            License: user.License || '',
+            birthdate: user.birthdate || '',
+            rentalHistory: user.rentalHistory || ''
         });
     };
 
@@ -107,9 +154,10 @@ const AdminDashboard = () => {
             console.log('Response body:', responseBody);
             if (response.ok) {
                 // Handle success, maybe show a success message
-                console.log('Vehicle created successfully');
-                // Fetch vehicles data after submission
-                fetchVehicles();
+                console.log('Vehicle created successfully');              
+                setVehicleError(null);// Reset error state               
+                setShowSuccessPopup(true);// Show success popup
+                fetchVehicles();// Fetch vehicles data after submission
             } else {
                 // Handle error, maybe show an error message
                 console.error('Failed to create vehicle');
@@ -130,7 +178,7 @@ const AdminDashboard = () => {
                 rentalHistory: []
             };
     
-            const response = await fetch('/api/users', {
+            const response = await fetch('/api/users/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -143,13 +191,12 @@ const AdminDashboard = () => {
                 const contentType = response.headers.get('content-type');
                 if (contentType && contentType.includes('application/json')) {
                     // Parse JSON response
-                    const responseBody = await response.json();
+                    const responseBody = await response.json(); 
                     console.log('Response body:', responseBody);
-    
-                    // Handle success, maybe show a success message
-                    console.log('User created successfully');
-                    // Fetch users data after submission
-                    fetchUsers();
+                    console.log('User created successfully'); // Handle success, maybe show a success message
+                    setUserError(null); // Reset error state
+                    setShowSuccessPopup(true); // Show success popup
+                    fetchUsers(); // Fetch users data after submission
                 } else {
                     console.error('Invalid content type in response:', contentType);
                     setUserError('Failed to create user. Invalid response from server.');
@@ -165,9 +212,98 @@ const AdminDashboard = () => {
         }
     };
 
+    const handleEditVehicleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`/api/vehicles/${editedVehicle._id}`, {
+                method: 'PATCH', // Use PATCH method for updating
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            console.log(response); //for debugging
+            const responseBody = await response.json();
+            console.log('Response body:', responseBody);
+            if (response.ok) {
+                // Handle success, maybe show a success message
+                console.log('Vehicle updated successfully');
+                setVehicleError(null); // Reset error state
+                setShowSuccessPopup(true); // Show success popup
+                fetchVehicles(); // Fetch vehicles data after submission
+            } else {
+                // Handle error, maybe show an error message
+                console.error('Failed to update vehicle');
+                setVehicleError('Failed to update vehicle. Please try again later.');
+            }
+        } catch (error) {
+            console.error('Error updating vehicle:', error);
+            setVehicleError('Failed to update vehicle. Please try again later.');
+        }
+    };
+
+    const handleEditUserSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            // Set rentalHistory to null in userFormData
+            const updatedUserFormData = {
+                ...userFormData,
+                rentalHistory: []
+            };
+    
+            const response = await fetch(`/api/users/${editedUser._id}`, {
+                method: 'PATCH', // Use PATCH method for updating
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updatedUserFormData)
+            });
+    
+            if (response.ok) {
+                // Check content type before parsing
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json')) {
+                    // Parse JSON response
+                    const responseBody = await response.json();
+                    console.log('Response body:', responseBody);
+                    console.log('User updated successfully'); // Handle success, maybe show a success message
+                    setUserError(null); // Reset error state
+                    setShowSuccessPopup(true); // Show success popup
+                    fetchUsers(); // Fetch users data after submission
+                } else {
+                    console.error('Invalid content type in response:', contentType);
+                    setUserError('Failed to update user. Invalid response from server.');
+                }
+            } else {
+                // Handle error, maybe show an error message
+                console.error('Failed to update user:', response.statusText);
+                setUserError('Failed to update user. Please try again later.');
+            }
+        } catch (error) {
+            console.error('Error updating user:', error);
+            setUserError('Failed to update user. Please try again later.');
+        }
+    };
+
+    const SuccessPopup = ({ message }) => {
+        return (
+            <div className="success-popup">
+                <p>{message}</p>
+            </div>
+        );
+    };
+    
+    SuccessPopup.propTypes = {
+        message: PropTypes.string.isRequired // Validate that message is a string and is required
+    };
+
     // Function to handle deleting a vehicle by ID
     const handleDeleteVehicle = async (id) => {
         try {
+            //remove dit form if present
+            setShowVEdit(false);
+            setShowUEdit(false);
+
             const response = await fetch(`/api/vehicles/${id}`, {
                 method: 'DELETE'
             });
@@ -188,6 +324,10 @@ const AdminDashboard = () => {
     // Function to handle deleting a user by ID
     const handleDeleteUser = async (id) => {
         try {
+            //remove dit form if present
+            setShowVEdit(false);
+            setShowUEdit(false);
+
             const response = await fetch(`/api/users/${id}`, {
             method: 'DELETE'
             });
@@ -208,12 +348,6 @@ const AdminDashboard = () => {
 
     return (
         <div className="dark:bg-black dark:text-white duration-300">
-        <div>
-      {/* UI elements */}
-      {vehicleError && <div className="error-message">{vehicleError}</div>}
-      {userError && <div className="error-message">{userError}</div>}
-      {/* More UI elements */}
-    </div>
             <div className="container mx-auto py-8">
                 <div className="flex justify-between mb-8">
                     <div>
@@ -259,7 +393,7 @@ const AdminDashboard = () => {
             </div>
             <div>
                 <label htmlFor="mileage">Mileage <hr></hr> </label>
-                <input type="number" id="mileage" name="mileage" value={formData.mileage} onChange={handleChange} required />
+                <input type="number" id="mileage" name="mileage" min="3" value={formData.mileage} onChange={handleChange} required />
             </div>
             <div>
                 <label htmlFor="transmission">Transmission <hr></hr> </label>
@@ -290,7 +424,8 @@ const AdminDashboard = () => {
             </div>
             <div>
                 <label htmlFor="seats">Seats <hr></hr> </label>
-                <input type="number" id="seats" name="seats" value={formData.seats} onChange={handleChange} required />
+                <input type="range" id="seats" name="seats" className="slider" min="1" max="8" value={formData.seats} onChange={handleChange} required />
+                <span>{formData.seats}</span>
             </div>
             <div>
                 <label htmlFor="pricePerDay">Price Per Day <hr></hr> </label>
@@ -306,6 +441,8 @@ const AdminDashboard = () => {
             </div>
             <br></br>
             <button className="formButton" type="submit">Submit</button>
+            {showSuccessPopup && <SuccessPopup message="Vehicle created successfully!" />}
+
         </form>
                     </div>
                 )}
@@ -343,7 +480,84 @@ const AdminDashboard = () => {
                             </div>
                             <br></br>
                             <button className="formButton" type="submit">Submit</button>
+                            {showSuccessPopup && <SuccessPopup message="User was created successfully!" />}
                         </form>
+                    </div>
+                )}
+
+                {/* Display editing forms when corresponding "Edit" buttons are clicked */}
+                {showVEdit && (
+                    <div>
+                        <form onSubmit={handleEditVehicleSubmit}>
+                    <div>
+                        <label htmlFor="color">Color <hr></hr> </label>
+                        <input type="text" id="color" name="color" value={formData.color} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="mileage">Mileage <hr></hr> </label>
+                        <input type="number" id="mileage" name="mileage" value={formData.mileage} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="location">Location <hr></hr> </label>
+                        <select id="location" name="location" value={formData.location} onChange={handleChange} required>
+                            <option value="">Select Location</option>
+                            {['Montreal', 'Ottawa', 'Toronto', 'Vancouver', 'Halifax', 'Edmonton'].map((location) => (
+                            <option key={location} value={location}>{location}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="pricePerDay">Price Per Day <hr></hr> </label>
+                        <input type="number" id="pricePerDay" name="pricePerDay" value={formData.pricePerDay} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="image">Image Link<hr></hr> </label>
+                        <input type="text" id="image" name="image" value={formData.image} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description <hr></hr> </label>
+                        <textarea id="description" name="description" value={formData.description} onChange={handleChange} />
+                    </div >
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <button type="submit"className="formButton">Update Vehicle</button>
+                    </div>
+                        </form>
+                        <br></br>
+                    </div>
+                )}
+
+                {showUEdit && (
+                    <div>
+                        <form onSubmit={handleEditUserSubmit}>
+                            <div>
+                                <label htmlFor="email">Email <hr></hr> </label>
+                                <input type="email" id="email" name="email" value={userFormData.email} onChange={handleUserChange} />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password <hr></hr> </label>
+                                <input type="password" id="password" name="password" value={userFormData.password} onChange={handleUserChange} />
+                            </div>
+                            <div>
+                                <label htmlFor="username">Username <hr></hr> </label>
+                                <input type="text" id="username" name="username" value={userFormData.username} onChange={handleUserChange} />
+                            </div>
+                            <div>
+                                <label htmlFor="permission">Permission <hr /></label>
+                                <select id="permission" name="permission" value={userFormData.permission} onChange={handleUserChange}>
+                                    <option value="Customer">Customer</option>
+                                    <option value="CSR">CSR</option>
+                                    <option value="Admin">Admin</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label htmlFor="License">License <hr></hr> </label>
+                                <input type="text" id="License" name="License" value={userFormData.License} onChange={handleUserChange} />
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <button type="submit"className="formButton">Update User</button>
+                            </div>
+                        </form>
+                        <br></br>
                     </div>
                 )}
 
@@ -390,7 +604,7 @@ const AdminDashboard = () => {
                                         <td>{vehicle.pricePerDay}</td>
                                         <td>{vehicle.description.split(' ').slice(0, 7).join(' ')}...</td> {/* Show only the first 7 words of the description */}
                                         <td>
-                                            <button className="formButton">Edit</button>
+                                            <button className="formButton" onClick={() => handleEditVehicle(vehicle)}>Edit</button>
                                             <br/><br/>
                                             <button className="formButton" onClick={() => handleDeleteVehicle(vehicle._id)}>Delete</button>
                                         </td>
@@ -428,7 +642,7 @@ const AdminDashboard = () => {
                                         <td>{user.License}</td>
                                         <td>{user.birthdate}</td>
                                         <td>
-                                            <button className="formButton">Edit</button>
+                                            <button className="formButton" onClick={() => handleEditUser(user)}>Edit</button>
                                             <br/><br/>
                                             <button className="formButton" onClick={() => handleDeleteUser(user._id)}>Delete</button>
                                         </td>
