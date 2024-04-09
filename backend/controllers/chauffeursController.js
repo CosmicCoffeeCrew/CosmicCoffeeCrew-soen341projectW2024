958b6c312579e2ad3ac595f21fce64e45b8ab984
Sara Amri <93613026+kokkuri3@users.noreply.github.com>
2024-04-09 11:46:53 +0900

    Update chauffeursController.js

--- backend/controllers/chauffeursController.js
+++ backend/controllers/chauffeursController.js
@@ -7,15 +7,15 @@ const mongoose = require('mongoose')
 
 // Function to create a new user
 const createChauffeur = async (req, res) => {
-    const { email, firstName, lastName, age, sex, contactNumber, location, description, pricePerDay, image} = req.body;
+    const { email, firstName, lastName, age, sex, contactNumber, location, description, pricePerHour, image} = req.body;
     try {
         // Check if the required fields are provided
-        if (!email || !firstName || !lastName || !age || !contactNumber || !sex || !location || !description || !pricePerDay || !image) {
+        if (!email || !firstName || !lastName || !age || !contactNumber || !sex || !location || !description || !pricePerHour || !image) {
             throw new Error("All fields must be filled");
         }
 
         // Create the user
-        const chauffeur = await Chauffeur.create({ email, firstName, lastName, age, sex, contactNumber, location, description, pricePerDay, image});
+        const chauffeur = await Chauffeur.create({ email, firstName, lastName, age, sex, contactNumber, location, description, pricePerHour, image});
 
         // Return success response with token
         res.status(200).json({chauffeur});
